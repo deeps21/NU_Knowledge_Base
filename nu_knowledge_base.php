a38e88162a31eef34916e062c312c13da048f2d4
Deepak Chauhan <71959891+deeps21@users.noreply.github.com>
2022-05-30 00:56:51 +0530

    Update nu_knowledge_base.php

--- nu_knowledge_base.php
+++ nu_knowledge_base.php
@@ -12,15 +12,15 @@
 
 
 
-define('MINERVA_KB_VERSION', '1.0');
-define('MINERVA_KB_OPTION_PREFIX', 'mkb_option_');
-define('MINERVA_KB_PLUGIN_URL', plugin_dir_url( __FILE__ ));
-define('MINERVA_KB_IMG_URL', MINERVA_KB_PLUGIN_URL . 'assets/img/');
-define('MINERVA_KB_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('MINERVA_THEME_DIR', get_template_directory());
+define('NU_KNOWLEDGE_BASE', '1.0');
+define('NU_KNOWLEDGE_BASE_OPTION_PREFIX', 'mkb_option_');
+define('NU_KNOWLEDGE_BASE_URL', plugin_dir_url( __FILE__ ));
+define('NU_KNOWLEDGE_BASE_IMG_URL', NU_KNOWLEDGE_BASE . 'assets/img/');
+define('NU_KNOWLEDGE_BASE_DIR', plugin_dir_path(__FILE__));
+define('NU_KNOWLEDGE_BASE', get_template_directory());
 
 // init app
-require_once(MINERVA_KB_PLUGIN_DIR . 'lib/app.php');
+require_once(NU_KNOWLEDGE_BASE . 'lib/app.php');
 
-global $minerva_kb;
-$minerva_kb = new MinervaKB_App();
+global $nu_kb;
+$nu_kb = new NU_KNOWLEDGE_BASE_App();

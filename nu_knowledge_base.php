bed2b7caeaeb06a60bb59bf30bc18cc189574494
Deepak Chauhan <deepakchauhan@Deepaks-MacBook-Air.local>
2022-05-30 00:08:09 +0530

    initial uploads

---
+++ nu_knowledge_base.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*NU Knowledge Base
+ */
+/**
+*Team Members
+*Deepak
+*Soham Pal
+*Yash Nehra
+*Drishti
+ */
+
+
+
+define('MINERVA_KB_VERSION', '1.0');
+define('MINERVA_KB_OPTION_PREFIX', 'mkb_option_');
+define('MINERVA_KB_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define('MINERVA_KB_IMG_URL', MINERVA_KB_PLUGIN_URL . 'assets/img/');
+define('MINERVA_KB_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('MINERVA_THEME_DIR', get_template_directory());
+
+// init app
+require_once(MINERVA_KB_PLUGIN_DIR . 'lib/app.php');
+
+global $minerva_kb;
+$minerva_kb = new MinervaKB_App();
